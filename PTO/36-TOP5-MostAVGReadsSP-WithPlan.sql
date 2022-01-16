18074af897cc90348deb92092db9866696fcd7b3
elvinikhsan <elvinikhsan@outlook.com>
2022-01-16 16:05:43 +0700

    remove extra queries

--- PTO/36-TOP5-MostAVGReadsSP-WithPlan.sql
+++ PTO/36-TOP5-MostAVGReadsSP-WithPlan.sql
@@ -18,31 +18,3 @@ WHERE (a.database_id > 4 AND DB_NAME(database_id) <> 'Resource')
 AND qp.query_plan IS NOT NULL
 ORDER BY AVG_LOGICAL_READS DESC
 GO
-SELECT TOP(5) p.name AS [SP Name], qs.total_logical_reads AS [TotalLogicalReads], 
-qs.total_logical_reads/qs.execution_count AS [AvgLogicalReads],qs.execution_count, 
-ISNULL(qs.execution_count/DATEDIFF(Minute, qs.cached_time, GETDATE()), 0) AS [Calls/Minute], 
-qs.total_elapsed_time, qs.total_elapsed_time/qs.execution_count AS [avg_elapsed_time],
-CASE WHEN CONVERT(nvarchar(max), qp.query_plan) LIKE N'%<MissingIndexes>%' THEN 1 ELSE 0 END AS [Has Missing Index], 
-FORMAT(qs.last_execution_time, 'yyyy-MM-dd HH:mm:ss', 'en-US') AS [Last Execution Time], 
-FORMAT(qs.cached_time, 'yyyy-MM-dd HH:mm:ss', 'en-US') AS [Plan Cached Time]
-FROM sys.procedures AS p WITH (NOLOCK)
-INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK)
-ON p.[object_id] = qs.[object_id]
-CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) AS qp
-WHERE qs.database_id = DB_ID()
-AND DATEDIFF(Minute, qs.cached_time, GETDATE()) > 0
-ORDER BY qs.total_logical_reads DESC OPTION (RECOMPILE);
-GO
-SELECT TOP(5) p.name AS [SP Name],qs.total_physical_reads AS [TotalPhysicalReads], 
-qs.total_physical_reads/qs.execution_count AS [AvgPhysicalReads], qs.execution_count, 
-qs.total_logical_reads,qs.total_elapsed_time, qs.total_elapsed_time/qs.execution_count AS [avg_elapsed_time],
-CASE WHEN CONVERT(nvarchar(max), qp.query_plan) LIKE N'%<MissingIndexes>%' THEN 1 ELSE 0 END AS [Has Missing Index],
-FORMAT(qs.last_execution_time, 'yyyy-MM-dd HH:mm:ss', 'en-US') AS [Last Execution Time], 
-FORMAT(qs.cached_time, 'yyyy-MM-dd HH:mm:ss', 'en-US') AS [Plan Cached Time]
-FROM sys.procedures AS p WITH (NOLOCK)
-INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK)
-ON p.[object_id] = qs.[object_id]
-CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) AS qp
-WHERE qs.database_id = DB_ID()
-AND qs.total_physical_reads > 0
-ORDER BY qs.total_physical_reads DESC, qs.total_logical_reads DESC OPTION (RECOMPILE);
--- PTO/37-TOP5-MostAVGCPUSP-WithPlan.sql
+++ PTO/37-TOP5-MostAVGCPUSP-WithPlan.sql
@@ -18,17 +18,4 @@ WHERE (a.database_id > 4 AND DB_NAME(database_id) <> 'Resource')
 AND qp.query_plan IS NOT NULL
 ORDER BY AVG_CPU DESC;
 GO
-SELECT TOP(25) p.name AS [SP Name], qs.total_worker_time AS [TotalWorkerTime], 
-qs.total_worker_time/qs.execution_count AS [AvgWorkerTime], qs.execution_count, 
-ISNULL(qs.execution_count/DATEDIFF(Minute, qs.cached_time, GETDATE()), 0) AS [Calls/Minute],
-qs.total_elapsed_time, qs.total_elapsed_time/qs.execution_count AS [avg_elapsed_time],
-CASE WHEN CONVERT(nvarchar(max), qp.query_plan) LIKE N'%<MissingIndexes>%' THEN 1 ELSE 0 END AS [Has Missing Index],
-FORMAT(qs.last_execution_time, 'yyyy-MM-dd HH:mm:ss', 'en-US') AS [Last Execution Time], 
-FORMAT(qs.cached_time, 'yyyy-MM-dd HH:mm:ss', 'en-US') AS [Plan Cached Time]
-FROM sys.procedures AS p WITH (NOLOCK)
-INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK)
-ON p.[object_id] = qs.[object_id]
-CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) AS qp
-WHERE qs.database_id = DB_ID()
-AND DATEDIFF(Minute, qs.cached_time, GETDATE()) > 0
-ORDER BY qs.total_worker_time DESC OPTION (RECOMPILE);
+
