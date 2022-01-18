5f4e6dd79a9672a614cf4be981a15af15fab563d
elvinikhsan <elvinikhsan@outlook.com>
2022-01-18 15:40:56 +0700

    .

--- PTO/44-CheckForSQLTriggers.sql
+++ PTO/44-CheckForSQLTriggers.sql
@@ -1,19 +1,55 @@
-DECLARE @QRY VARCHAR(MAX) ='';
+DECLARE @dbname SYSNAME; 
+DECLARE @sqlcommand AS TABLE (ID INT IDENTITY(1,1) PRIMARY KEY CLUSTERED, CommandText VARCHAR(MAX));
+DECLARE @databases AS TABLE (ID INT IDENTITY(1,1) PRIMARY KEY CLUSTERED, DBName SYSNAME);
+DECLARE @temp AS TABLE 
+		(ID INT IDENTITY(1,1) PRIMARY KEY CLUSTERED
+		,[DatabaseName] SYSNAME
+		--,SchemaName VARCHAR(100)
+		,[TableName] SYSNAME
+		,TriggerName SYSNAME
+		,ParentClassDesc SYSNAME
+		,TypeDesc SYSNAME
+		,CreatedDate DATETIME
+		,IsDisabled BIT);
 
-SELECT @QRY =@QRY + ' select  ' + QUOTENAME(name,'''') + ' COLLATE SQL_Latin1_General_CP1_CI_AS AS database_name 
-    ,OBJECT_NAME(TR.parent_id) COLLATE SQL_Latin1_General_CP1_CI_AS AS table_name 
-	,name COLLATE SQL_Latin1_General_CP1_CI_AS AS trigger_name
-	,parent_class_desc
-	,type_desc
-	,create_date
-	,is_disabled
-FROM ['+name+'].SYS.TRIGGERS TR
-UNION ALL
-' 
-FROM SYS.DATABASES
-WHERE name not IN ('master', 'model', 'msdb', 'tempdb', 'resource',
-       'distribution' , 'reportserver', 'reportservertempdb','jiradb')
+DECLARE @count INT = 0;
+DECLARE @j INT =1;
 
-SELECT @QRY = SUBSTRING(@QRY,1,LEN(@QRY)-12)
+INSERT INTO @databases SELECT name 
+FROM master.dbo.sysdatabases 
+WHERE name NOT IN ('master','model','msdb','tempdb')  
+ORDER BY name;
 
-EXEC( @QRY)
+SELECT @count=COUNT(*) FROM @databases;
+
+-- generate commands and store it in @sqlcommand
+WHILE @j <= @count
+BEGIN  
+		DECLARE @sqltext VARCHAR(MAX)='';
+		SELECT @dbname = DBName FROM @databases WHERE ID=@j;
+		SET @sqltext = 'USE ' + QUOTENAME(@dbname) + ';
+						SELECT  ''' + REPLACE(@dbname, CHAR(39), CHAR(95)) + ''' AS [DatabaseName]
+								,ISNULL(OBJECT_NAME(parent_id),'''') AS table_name 
+								,name AS trigger_name
+								,parent_class_desc
+								,type_desc
+								,create_date
+								,is_disabled
+						FROM sys.triggers;'
+
+		INSERT INTO @sqlcommand(CommandText) VALUES (@sqltext);
+		SET @j=@j+1;
+END  
+
+--execute the commands
+SET @j=1;
+WHILE @j<= @count
+BEGIN
+	DECLARE @sqlcmd NVARCHAR(MAX)='';
+	SELECT @sqlcmd = CommandText FROM @sqlcommand WHERE ID=@j;
+	INSERT INTO @temp
+	EXEC (@sqlcmd);
+	SET @j=@j+1;
+END
+
+SELECT * FROM @temp ORDER BY ID;
