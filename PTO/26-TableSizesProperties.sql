658f86c6a3cca01fa976f4cd8e9c8b8d5d1bd8c0
elvinikhsan <elvinikhsan@outlook.com>
2022-03-03 10:16:01 +0700

    update scripts table size/properties for all db

--- PTO/26-TableSizesProperties.sql
+++ PTO/26-TableSizesProperties.sql
@@ -1,30 +1,59 @@
-SELECT SCHEMA_NAME(o.Schema_ID) AS [Schema Name], OBJECT_NAME(p.object_id) AS [ObjectName], 
-SUM(p.Rows) AS [RowCount], data_compression_desc AS [CompressionType]
-FROM sys.partitions AS p WITH (NOLOCK)
-INNER JOIN sys.objects AS o WITH (NOLOCK)
-ON p.object_id = o.object_id
-WHERE index_id < 2 --ignore the partitions from the non-clustered index if any
-AND OBJECT_NAME(p.object_id) NOT LIKE N'sys%'
-AND OBJECT_NAME(p.object_id) NOT LIKE N'spt_%'
-AND OBJECT_NAME(p.object_id) NOT LIKE N'queue_%' 
-AND OBJECT_NAME(p.object_id) NOT LIKE N'filestream_tombstone%' 
-AND OBJECT_NAME(p.object_id) NOT LIKE N'fulltext%'
-AND OBJECT_NAME(p.object_id) NOT LIKE N'ifts_comp_fragment%'
-AND OBJECT_NAME(p.object_id) NOT LIKE N'filetable_updates%'
-AND OBJECT_NAME(p.object_id) NOT LIKE N'xml_index_nodes%'
-AND OBJECT_NAME(p.object_id) NOT LIKE N'sqlagent_job%'
-AND OBJECT_NAME(p.object_id) NOT LIKE N'plan_persist%'
-GROUP BY  SCHEMA_NAME(o.Schema_ID), p.object_id, data_compression_desc
-ORDER BY SUM(p.Rows) DESC OPTION (RECOMPILE);
-GO
---Table properties
-SELECT OBJECT_NAME(t.[object_id]) AS [ObjectName], p.[rows] AS [Table Rows], p.index_id, 
-       p.data_compression_desc AS [Index Data Compression],
-       t.create_date, t.lock_on_bulk_load, t.is_replicated, t.has_replication_filter, 
-       t.is_tracked_by_cdc, t.lock_escalation_desc, t.is_filetable, 
-	   t.is_memory_optimized, t.durability_desc  -- new for SQL Server 2014
-FROM sys.tables AS t WITH (NOLOCK)
-INNER JOIN sys.partitions AS p WITH (NOLOCK)
-ON t.[object_id] = p.[object_id]
-WHERE OBJECT_NAME(t.[object_id]) NOT LIKE N'sys%'
-ORDER BY OBJECT_NAME(t.[object_id]), p.index_id OPTION (RECOMPILE);
+DECLARE @dbname SYSNAME; 
+DECLARE @sqlcommand AS TABLE (ID INT IDENTITY(1,1) PRIMARY KEY CLUSTERED, CommandText VARCHAR(MAX));
+DECLARE @databases AS TABLE (ID INT IDENTITY(1,1) PRIMARY KEY CLUSTERED, DBName SYSNAME);
+DECLARE @temp AS TABLE 
+		(ID INT IDENTITY(1,1) PRIMARY KEY CLUSTERED
+		,[DatabaseName] SYSNAME
+		,SchemaName SYSNAME NULL
+		,ObjectName SYSNAME NULL
+		,[RowCount] BIGINT NULL
+		,TypeDesc VARCHAR(25)
+		,IndexID INT NULL
+		,DataCompression VARCHAR(25) NULL
+		,CreatedDate DATETIME NULL
+		,IsReplicated BIT NULL
+		,IsTrackedByCDC BIT NULL
+		,LockEscalationDesc VARCHAR(25) NULL);
+
+DECLARE @count INT = 0;
+DECLARE @j INT =1;
+
+INSERT INTO @databases SELECT name 
+FROM master.dbo.sysdatabases 
+WHERE name NOT IN ('master','model','msdb','tempdb')  
+ORDER BY name;
+
+SELECT @count=COUNT(*) FROM @databases;
+
+-- generate commands and store it in @sqlcommand
+WHILE @j <= @count
+BEGIN  
+		DECLARE @sqltext VARCHAR(MAX)='';
+		SELECT @dbname = DBName FROM @databases WHERE ID=@j;
+		SET @sqltext = 'USE ' + QUOTENAME(@dbname) + ';
+						SELECT ''' + REPLACE(@dbname, CHAR(39), CHAR(95)) + ''' AS [DatabaseName], SCHEMA_NAME(t.schema_id) AS SchemaName, t.name AS ObjectName, p.[rows] AS [RowCount], 
+							   t.type_desc, p.index_id AS IndexID, p.data_compression_desc AS [DataCompression], t.create_date AS CreatedDate, t.is_replicated AS IsReplicated, 
+							   t.is_tracked_by_cdc AS IsTrackedByCDC, t.lock_escalation_desc AS LockEscalationMode
+						FROM sys.tables AS t WITH (NOLOCK)
+						INNER JOIN sys.partitions AS p WITH (NOLOCK)
+						ON t.[object_id] = p.[object_id]
+						WHERE OBJECT_NAME(t.[object_id]) NOT LIKE N''sys%'' AND rows >= 100000
+						ORDER BY p.[rows] DESC, t.name, p.index_id OPTION (RECOMPILE);'
+
+		INSERT INTO @sqlcommand(CommandText) VALUES (@sqltext);
+		SET @j=@j+1;
+END  
+
+--execute the commands
+SET @j=1;
+WHILE @j<= @count
+BEGIN
+	DECLARE @sqlcmd NVARCHAR(MAX)='';
+	SELECT @sqlcmd = CommandText FROM @sqlcommand WHERE ID=@j;
+	INSERT INTO @temp
+	EXEC (@sqlcmd);
+	SET @j=@j+1;
+END
+
+SELECT * FROM @temp ORDER BY ID;
+
