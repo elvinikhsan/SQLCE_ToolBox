6854d1ed16d49ab47d6531cdbb2a6f3acecb2fcd
elvinikhsan <elvinikhsan@outlook.com>
2024-02-16 13:48:50 +0700

    add script check disabled indexes

---
+++ PTO/66-CheckDisabledIndexes.sql
@@ -0,0 +1,58 @@
+DECLARE @dbname SYSNAME; 
+DECLARE @sqlcommand AS TABLE (ID INT IDENTITY(1,1) PRIMARY KEY CLUSTERED, CommandText VARCHAR(MAX));
+DECLARE @databases AS TABLE (ID INT IDENTITY(1,1) PRIMARY KEY CLUSTERED, DBName SYSNAME);
+DECLARE @temp AS TABLE 
+		(ID INT IDENTITY(1,1) PRIMARY KEY CLUSTERED
+		,[DatabaseName] SYSNAME
+		--,SchemaName VARCHAR(100)
+		,[TableName] VARCHAR(MAX)
+		,IndexName VARCHAR(MAX)
+		,IndexType NVARCHAR(20)
+		,IndexSize BIGINT
+		,Is_Disabled BIT);
+
+DECLARE @count INT = 0;
+DECLARE @j INT =1;
+
+INSERT INTO @databases SELECT name 
+FROM master.dbo.sysdatabases 
+WHERE name NOT IN ('master','model','msdb','tempdb')  
+ORDER BY name;
+
+SELECT @count=COUNT(*) FROM @databases;
+
+-- generate commands and store it in @sqlcommand
+WHILE @j <= @count
+BEGIN  
+		DECLARE @sqltext VARCHAR(MAX)='';
+		SELECT @dbname = DBName FROM @databases WHERE ID=@j;
+		SET @sqltext = 'USE ' + QUOTENAME(@dbname) + ';
+						SELECT DISTINCT ''' + REPLACE(@dbname, CHAR(39), CHAR(95)) + ''' AS [DatabaseName], 
+						OBJECT_NAME(IX.OBJECT_ID) Table_Name
+					   ,IX.name AS Index_Name
+					   ,IX.type_desc Index_Type
+					   ,SUM(PS.[used_page_count]) * 8 IndexSizeKB
+					   ,IX.is_disabled
+				FROM sys.indexes IX
+				INNER JOIN sys.dm_db_partition_stats PS on PS.object_id=IX.object_id
+				WHERE OBJECTPROPERTY(IX.OBJECT_ID,''IsUserTable'') = 1
+				AND IX.index_id > 1 AND IX.[type_desc] = N''NONCLUSTERED''
+				AND IX.is_disabled = 1
+				GROUP BY OBJECT_NAME(IX.OBJECT_ID) ,IX.name ,IX.type_desc, IX.is_disabled;'
+
+		INSERT INTO @sqlcommand(CommandText) VALUES (@sqltext);
+		SET @j=@j+1;
+END  
+
+--execute the commands
+SET @j=1;
+WHILE @j<= @count
+BEGIN
+	DECLARE @sqlcmd NVARCHAR(MAX)='';
+	SELECT @sqlcmd = CommandText FROM @sqlcommand WHERE ID=@j;
+	INSERT INTO @temp
+	EXEC (@sqlcmd);
+	SET @j=@j+1;
+END
+
+SELECT * FROM @temp ORDER BY ID;
