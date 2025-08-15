f11bd54f44ddb0aa4cdfebd1e7e690005782b67c
Elvin Ikhsan <elvin.ikhsan@hotmail.com>
2025-08-15 19:32:50 +0700

    Modify GetAllDatabaseDataLogSize

--- PTO/60-GetAllDatabaseDataLogSize.sql
+++ PTO/60-GetAllDatabaseDataLogSize.sql
@@ -1,11 +1,32 @@
-with fs
-as
-(
-    select database_id, type, size * 8.0 / 1024 size
-    from sys.master_files
-)
-select 
-    name,
-    (select sum(size) from fs where type = 0 and fs.database_id = db.database_id) DataFileSizeMB,
-    (select sum(size) from fs where type = 1 and fs.database_id = db.database_id) LogFileSizeMB
-from sys.databases db
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
+
+CREATE TABLE #FileSize
+(DBName SYSNAME, 
+    FileName SYSNAME, 
+    FileType SYSNAME,
+    CurrentSizeMB DECIMAL(18,2), 
+	SpaceUsedMB DECIMAL(18,2),
+    FreeSpaceMB DECIMAL(18,2)
+);
+    
+INSERT INTO #FileSize(dbName, FileName, FileType, CurrentSizeMB, SpaceUsedMB, FreeSpaceMB)
+EXEC sp_msforeachdb 
+'USE [?]; 
+ SELECT DB_NAME() AS DbName, 
+        name AS FileName, 
+        type_desc,
+        size/128.0 AS CurrentSizeMB,  
+		CAST(FILEPROPERTY(name, ''SpaceUsed'') AS INT)/128.0 AS UsedSpaceMB,
+        size/128.0 - CAST(FILEPROPERTY(name, ''SpaceUsed'') AS INT)/128.0 AS FreeSpaceMB
+FROM sys.database_files
+WHERE type IN (0,1);';
+    
+SELECT DBName, FileType, SUM(CurrentSizeMB) AS CurrentSizeMB, SUM(SpaceUsedMB) AS SpaceUsedMB, SUM(FreeSpaceMB) AS FreeSpaceMB
+FROM #FileSize
+WHERE DBName NOT IN ('distribution', 'master', 'model', 'msdb')
+GROUP BY DBName, FileType
+ORDER BY DBName, FileType DESC;
+    
+DROP TABLE #FileSize;
+GO
