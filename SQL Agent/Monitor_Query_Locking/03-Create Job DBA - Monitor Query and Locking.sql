f3467567e30930d1212649c0197d16abeb4deef8
Elvin Ikhsan <elvin.ikhsan@hotmail.com>
2025-09-16 14:16:09 +0700

    update schedules for query monitoring jobs

--- SQL Agent/Monitor_Query_Locking/03-Create Job DBA - Monitor Query and Locking.sql
+++ SQL Agent/Monitor_Query_Locking/03-Create Job DBA - Monitor Query and Locking.sql
@@ -1,18 +1,13 @@
 USE [msdb]
 GO
-
-/****** Object:  Job [DBA - Monitor Queries and Locking]    Script Date: 7/21/2022 4:02:49 AM ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 7/21/2022 4:02:49 AM ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-
 END
-
 DECLARE @jobId BINARY(16)
 EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DBA - Monitor Queries and Locking', 
 		@enabled=1, 
@@ -25,7 +20,6 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DBA - Monitor Queries and Lo
 		@category_name=N'[Uncategorized (Local)]', 
 		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Exec sp_whoisactive]    Script Date: 7/21/2022 4:02:49 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Exec sp_whoisactive', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
@@ -47,11 +41,11 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'DBA - Mon
 		@freq_type=4, 
 		@freq_interval=1, 
 		@freq_subday_type=2, 
-		@freq_subday_interval=30, 
+		@freq_subday_interval=15, 
 		@freq_relative_interval=0, 
 		@freq_recurrence_factor=0, 
-		@active_start_date=20200218, 
-		@active_end_date=99991231, 
+		@active_start_date=20250924, 
+		@active_end_date=20250925, 
 		@active_start_time=0, 
 		@active_end_time=235900
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
@@ -63,5 +57,3 @@ QuitWithRollback:
     IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
 EndSave:
 GO
-
-
--- SQL Agent/Monitor_Query_Locking/04-Create Job DBA - Cleanup sp_whowasactive.sql
+++ SQL Agent/Monitor_Query_Locking/04-Create Job DBA - Cleanup sp_whowasactive.sql
@@ -1,18 +1,13 @@
 USE [msdb]
 GO
-
-/****** Object:  Job [DBA - Clean-up sp_WhoAasActive]    Script Date: 10/2/2020 4:28:28 PM ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 10/2/2020 4:28:28 PM ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-
 END
-
 DECLARE @jobId BINARY(16)
 EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DBA - Clean-up sp_WhoWasActive', 
 		@enabled=1, 
@@ -25,7 +20,6 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DBA - Clean-up sp_WhoWasActi
 		@category_name=N'[Uncategorized (Local)]', 
 		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Disable Job DBA - Monitor Queries and Locking]    Script Date: 10/2/2020 4:28:28 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Disable Job DBA - Monitor Queries and Locking', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
@@ -43,7 +37,6 @@ GO',
 		@database_name=N'master', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Truncate table sp_WhoWasActive]    Script Date: 10/2/2020 4:28:28 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Truncate table sp_WhoWasActive', 
 		@step_id=2, 
 		@cmdexec_success_code=0, 
@@ -59,7 +52,6 @@ TRUNCATE TABLE dbo.sp_whowasactive;',
 		@database_name=N'master', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Enable job DBA - Monitor Queries and Locking]    Script Date: 10/2/2020 4:28:28 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Enable job DBA - Monitor Queries and Locking', 
 		@step_id=3, 
 		@cmdexec_success_code=0, 
@@ -86,8 +78,8 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'DBA - Cle
 		@freq_subday_type=1, 
 		@freq_subday_interval=0, 
 		@freq_relative_interval=0, 
-		@freq_recurrence_factor=2, 
-		@active_start_date=20201002, 
+		@freq_recurrence_factor=1, 
+		@active_start_date=20251001, 
 		@active_end_date=99991231, 
 		@active_start_time=0, 
 		@active_end_time=235959
@@ -100,3 +92,5 @@ QuitWithRollback:
     IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
 EndSave:
 GO
+
+
